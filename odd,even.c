207f33a26552df9fa183d630657988c512d5cd43
Abhishekjha9897 <abhishekjha332001@gmail.com>
2024-09-27 16:30:21 +0530

    Add files via upload

---
+++ odd,even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>;
+#include<math.h>
+void main (){
+	int n;
+	printf("enter a value \n");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("even");
+	}else{
+		printf("odd");
+	}
+	
+	
+}
+
+	
+   
