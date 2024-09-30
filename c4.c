f796acfaab2ce6c9e8156063b2e26971eb9487b1
Abhishekjha9897 <abhishekjha332001@gmail.com>
2024-09-30 22:37:03 +0530

    Add files via upload

---
+++ c4.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+int main() {
+int temp;
+printf("temp");
+scanf("%d",&temp);
+if(temp<=10)
+printf("very cool");
+ else if(temp<=20)
+ printf("cool");
+ else if(temp<=30)
+ printf("sunny");
+ else if(temp<=40)
+ printf("hot");
+ else if(temp=50)
+ printf("very hot");
+
+return 0;
+}
+
