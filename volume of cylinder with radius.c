83b00b3de3f25ac2385e5a9fced546c18760b79c
Abhishekjha9897 <abhishekjha332001@gmail.com>
2024-10-12 16:50:35 +0530

    Add files via upload

---
+++ volume of cylinder with radius.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int radius = 2;
+    int height = 2;
+    float pi;
+   // printf("enter radius ");
+   // scanf("%d",&radius);
+   // printf("enter height ");
+    //scanf("%d",&height);
+
+    printf("the volume of cylinder with radius and height is %d %f" ,3.14*radius*radius*height);
+
+  return 0;
+
+}
