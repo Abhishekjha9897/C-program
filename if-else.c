0d5e0f25006565daeee8bf3429680be2368699f3
Abhishekjha9897 <abhishekjha332001@gmail.com>
2024-11-13 18:35:36 +0530

    Add files via upload

---
+++ if-else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	 int a;
+	 printf("enter the value=");
+	 scanf("%d",&a);
+	 if(a>=0)
+	 {
+printf("enter number is positive");
+}
+else
+{
+    printf("enter number is negative");
+}
+	
+	
+	return 0;
+}
